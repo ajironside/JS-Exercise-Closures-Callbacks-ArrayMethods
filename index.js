a9a9d85e36824afb9bd898b36891e9f8ab70b820
Amanda Ironside <ironside.amanda@gmail.com>
2020-01-28 15:57:22 -0700

    completed shirt size function

--- index.js
+++ index.js
@@ -219,8 +219,11 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
  */
-function getRunnersByTShirtSize( /* CODE HERE */ ) {
-    /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+    let shirtSizes = runners.filter(function(checkShirt) {
+        return checkShirt.shirt_size == tShirtSize
+    });
+    return shirtSizes;
 }
 
 /**
@@ -233,9 +236,10 @@ function getRunnersByTShirtSize( /* CODE HERE */ ) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
  */
-function tallyUpDonations( /* CODE HERE */ ) {
-    /* CODE HERE */
+function tallyUpDonations(runners) {
+    const total = [];
 }
+
 
 /////////////// CLOSURES ///////////////
 /////////////// CLOSURES ///////////////
